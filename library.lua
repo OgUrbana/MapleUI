ec0d2041ad02c990050d3fbc5e20852171f4f4f3
OgUrbana <41495671+OgUrbana@users.noreply.github.com>
2022-10-17 18:40:52 -0400

    Update library.lua

--- library.lua
+++ library.lua
@@ -18,32 +18,35 @@ local Themes = {
 }
 
 function WindowTable:CreateWindow()
-    
     -- Instances:
+
     local Maple = Instance.new("ScreenGui")
     local MainUI = Instance.new("Frame")
-    local UICorner = Instance.new("UICorner")
+    local MainCorner = Instance.new("UICorner")
     local LeftTab = Instance.new("Frame")
-    local UICorner_2 = Instance.new("UICorner")
+    local LeftTabCorner = Instance.new("UICorner")
     local LogoName = Instance.new("TextLabel")
     local WelcomeWidget = Instance.new("Frame")
-    local UICorner_3 = Instance.new("UICorner")
-    local ImageLabel = Instance.new("ImageLabel")
-    local UICorner_4 = Instance.new("UICorner")
-    local UIPadding = Instance.new("UIPadding")
+    local WelcomeWidgetCorner = Instance.new("UICorner")
+    local UserImage = Instance.new("ImageLabel")
+    local userImageCorner = Instance.new("UICorner")
+    local WelcomeWidgetPadding = Instance.new("UIPadding")
     local WelcomeBack = Instance.new("TextLabel")
     local UserName = Instance.new("TextLabel")
     local LogoImage = Instance.new("ImageLabel")
-    local UIPadding_2 = Instance.new("UIPadding")
+    local LeftTabPadding = Instance.new("UIPadding")
+    local TabWidget = Instance.new("Frame")
+    local TabWidgetCorner = Instance.new("UICorner")
+    local TabWidgetPadding = Instance.new("UIPadding") 
     local FooterWidget = Instance.new("Frame")
-    local UICorner_7 = Instance.new("UICorner")
+    local FooterCorner = Instance.new("UICorner")
     local RightPages = Instance.new("Frame")
-    local UICorner_8 = Instance.new("UICorner")
+    local RightPagesCorner = Instance.new("UICorner")
 
     --Properties:
 
     Maple.Name = "Maple"
-    Maple.Parent = game.CoreGui
+    Maple.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
     Maple.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
     MainUI.Name = "MainUI"
@@ -52,16 +55,18 @@ function WindowTable:CreateWindow()
     MainUI.Position = UDim2.new(0.49934727, 0, 0.101251423, 0)
     MainUI.Size = UDim2.new(0, 725, 0, 600)
 
-    UICorner.CornerRadius = UDim.new(0, 15)
-    UICorner.Parent = MainUI
+    MainCorner.CornerRadius = UDim.new(0, 15)
+    MainCorner.Name = "MainCorner"
+    MainCorner.Parent = MainUI
 
     LeftTab.Name = "LeftTab"
     LeftTab.Parent = MainUI
     LeftTab.BackgroundColor3 = Color3.fromRGB(52, 52, 52)
     LeftTab.Size = UDim2.new(0, 265, 0, 600)
 
-    UICorner_2.CornerRadius = UDim.new(0, 15)
-    UICorner_2.Parent = LeftTab
+    LeftTabCorner.CornerRadius = UDim.new(0, 15)
+    LeftTabCorner.Name = "LeftTabCorner"
+    LeftTabCorner.Parent = LeftTab
 
     LogoName.Name = "LogoName"
     LogoName.Parent = LeftTab
@@ -80,21 +85,25 @@ function WindowTable:CreateWindow()
     WelcomeWidget.Position = UDim2.new(0.00407792768, 0, 0.0631578937, 0)
     WelcomeWidget.Size = UDim2.new(0, 232, 0, 87)
 
-    UICorner_3.CornerRadius = UDim.new(0, 15)
-    UICorner_3.Parent = WelcomeWidget
+    WelcomeWidgetCorner.CornerRadius = UDim.new(0, 15)
+    WelcomeWidgetCorner.Name = "WelcomeWidgetCorner"
+    WelcomeWidgetCorner.Parent = WelcomeWidget
 
-    ImageLabel.Parent = WelcomeWidget
-    ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    ImageLabel.Size = UDim2.new(0, 50, 0, 50)
-    ImageLabel.Image = "rbxassetid://11301508877"
+    UserImage.Name = "UserImage"
+    UserImage.Parent = WelcomeWidget
+    UserImage.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    UserImage.Size = UDim2.new(0, 50, 0, 50)
+    UserImage.Image = "rbxassetid://11301508877"
 
-    UICorner_4.CornerRadius = UDim.new(0, 15)
-    UICorner_4.Parent = ImageLabel
+    userImageCorner.CornerRadius = UDim.new(0, 15)
+    userImageCorner.Name = "userImageCorner"
+    userImageCorner.Parent = UserImage
 
-    UIPadding.Parent = WelcomeWidget
-    UIPadding.PaddingLeft = UDim.new(0, 18)
-    UIPadding.PaddingRight = UDim.new(0, 18)
-    UIPadding.PaddingTop = UDim.new(0, 18)
+    WelcomeWidgetPadding.Name = "WelcomeWidgetPadding"
+    WelcomeWidgetPadding.Parent = WelcomeWidget
+    WelcomeWidgetPadding.PaddingLeft = UDim.new(0, 18)
+    WelcomeWidgetPadding.PaddingRight = UDim.new(0, 18)
+    WelcomeWidgetPadding.PaddingTop = UDim.new(0, 18)
 
     WelcomeBack.Name = "WelcomeBack"
     WelcomeBack.Parent = WelcomeWidget
@@ -128,14 +137,29 @@ function WindowTable:CreateWindow()
     LogoImage.Size = UDim2.new(0, 31, 0, 30)
     LogoImage.Image = "rbxassetid://11301490394"
 
-    UIPadding_2.Parent = LeftTab
-    UIPadding_2.PaddingBottom = UDim.new(0, 15)
-    UIPadding_2.PaddingLeft = UDim.new(0, 15)
-    UIPadding_2.PaddingRight = UDim.new(0, 15)
-    UIPadding_2.PaddingTop = UDim.new(0, 15)
+    LeftTabPadding.Name = "LeftTabPadding"
+    LeftTabPadding.Parent = LeftTab
+    LeftTabPadding.PaddingBottom = UDim.new(0, 15)
+    LeftTabPadding.PaddingLeft = UDim.new(0, 15)
+    LeftTabPadding.PaddingRight = UDim.new(0, 15)
+    LeftTabPadding.PaddingTop = UDim.new(0, 15)
 
+    TabWidget.Name = "TabWidget"
+    TabWidget.Parent = LeftTab
+    TabWidget.BackgroundColor3 = Color3.fromRGB(46, 46, 46)
+    TabWidget.Position = UDim2.new(-5.10380196e-05, 0, 0.239322543, 0)
+    TabWidget.Size = UDim2.new(0, 232, 0, 379)
 
+    TabWidgetCorner.CornerRadius = UDim.new(0, 15)
+    TabWidgetCorner.Name = "TabWidgetCorner"
+    TabWidgetCorner.Parent = TabWidget
 
+    TabWidgetPadding.Name = "TabWidgetPadding"
+    TabWidgetPadding.Parent = TabWidget
+    TabWidgetPadding.PaddingBottom = UDim.new(0, 15)
+    TabWidgetPadding.PaddingLeft = UDim.new(0, 15)
+    TabWidgetPadding.PaddingRight = UDim.new(0, 15)
+    TabWidgetPadding.PaddingTop = UDim.new(0, 15)
 
     FooterWidget.Name = "FooterWidget"
     FooterWidget.Parent = LeftTab
@@ -143,8 +167,9 @@ function WindowTable:CreateWindow()
     FooterWidget.Position = UDim2.new(-5.16048312e-05, 0, 0.924534261, 0)
     FooterWidget.Size = UDim2.new(0, 232, 0, 44)
 
-    UICorner_7.CornerRadius = UDim.new(0, 15)
-    UICorner_7.Parent = FooterWidget
+    FooterCorner.CornerRadius = UDim.new(0, 15)
+    FooterCorner.Name = "FooterCorner"
+    FooterCorner.Parent = FooterWidget
 
     RightPages.Name = "RightPages"
     RightPages.Parent = MainUI
@@ -152,9 +177,9 @@ function WindowTable:CreateWindow()
     RightPages.Position = UDim2.new(0.386206895, 0, 0.0290475972, 0)
     RightPages.Size = UDim2.new(0, 430, 0, 567)
 
-    UICorner_8.CornerRadius = UDim.new(0, 15)
-    UICorner_8.Parent = RightPages
-
+    RightPagesCorner.CornerRadius = UDim.new(0, 15)
+    RightPagesCorner.Name = "RightPagesCorner"
+    RightPagesCorner.Parent = RightPages
 
 
     local pagesFolder = Instance.new("Folder")
@@ -165,42 +190,39 @@ function WindowTable:CreateWindow()
 
     function TabHandler:CreateTab(tabname)
         tabname = tabname or "New Tab"
-        local TabWidget = Instance.new("Frame") -- Widget for Tab
-        local UICorner_5 = Instance.new("UICorner") -- UI Corner for tab
-        local TabButton = Instance.new("TextButton") -- Tab button
-        local UICorner_6 = Instance.new("UICorner") -- Button UI Corner
-        local UIPadding_3 = Instance.new("UIPadding") -- Button Padding
+        local TabButton = Instance.new("TextButton") -- Tab Button
+        local TabButtonCorner = Instance.new("UICorner") -- Tab Button Corner
 
-        local newPage = Instance.new("ScrollingFrame") -- Page
-        local elementListing = Instance.new("UIListLayout") -- Part of Page
-        
-        -- Tab Widget
-        TabWidget.Name = "TabWidget"
-        TabWidget.Parent = LeftTab
-        TabWidget.BackgroundColor3 = Themes['Default'].ContainerColor
-        TabWidget.Position = UDim2.new(-5.10380196e-05, 0, 0.239322543, 0)
-        TabWidget.Size = UDim2.new(0, 232, 0, 379)
-
-        -- [CornerRadius] Tab Widget
-        UICorner_5.CornerRadius = UDim.new(0, 15)
-        UICorner_5.Parent = TabWidget
-
-        -- [UIPadding] Tab Widget
-        UIPadding_3.Parent = TabWidget
-        UIPadding_3.PaddingBottom = UDim.new(0, 15)
-        UIPadding_3.PaddingLeft = UDim.new(0, 15)
-        UIPadding_3.PaddingRight = UDim.new(0, 15)
-        UIPadding_3.PaddingTop = UDim.new(0, 15)
-
-        -- Tab Button
+        -- Tab Button Properties
         TabButton.Name = "TabButton"
         TabButton.Parent = TabWidget
         TabButton.BackgroundColor3 = Themes['Default'].PrimaryElementColor
         TabButton.Size = UDim2.new(0, 200, 0, 50)
         TabButton.Font = Enum.Font.Ubuntu
-        TabButton.Text = tabname
+        TabButton.Text = "Visuals"
         TabButton.TextColor3 = Color3.fromRGB(0, 0, 0)
         TabButton.TextSize = 22.000
+
+        -- Tab Button Corner Properties
+        TabButtonCorner.CornerRadius = UDim.new(0, 15)
+        TabButtonCorner.Name = "TabButtonCorner"
+        TabButtonCorner.Parent = TabButton
+
+        local newPage = Instance.new("ScrollingFrame") -- New Page
+        local elementListing = Instance.new("UIListLayout") -- New Page Element Listing
+
+        newPage.Name = "newPage"
+        newPage.Parent = RightPages
+        newPage.Active = true
+        newPage.BackgroundColor3 = Color3.fromRGB(46, 46, 46)
+        newPage.BackgroundTransparency = 1.000
+        newPage.BorderSizePixel = 0
+        newPage.Size = UDim2.new(0, 430, 0, 565)
+        newPage.ScrollBarThickness = 8
+    
+        elementListing.Name = "elementListing"
+        elementListing.Parent = newPage
+        elementListing.SortOrder = Enum.SortOrder.LayoutOrder
 
         TabButton.MouseButton1Click:Connect(function()
             for i, v in next, pagesFolder:GetChildren() do -- get all pages that we add
@@ -221,24 +243,24 @@ function WindowTable:CreateWindow()
             }):Play()
         end)
 
-        -- [UI Corner] Tab Button
-        UICorner_6.CornerRadius = UDim.new(0, 15)
-        UICorner_6.Parent = TabButton
+        -- -- [UI Corner] Tab Button
+        -- UICorner_6.CornerRadius = UDim.new(0, 15)
+        -- UICorner_6.Parent = TabButton
 
-        -- New Page
-        newPage.Name = "newPage"
-        newPage.Parent = RightPages
-        newPage.Active = true
-        newPage.BackgroundColor3 = Themes['Default'].ContainerColor
-        newPage.BackgroundTransparency = 1.000
-        newPage.BorderSizePixel = 0
-        newPage.Size = UDim2.new(0, 430, 0, 565)
-        newPage.ScrollBarThickness = 8
+        -- -- New Page
+        -- newPage.Name = "newPage"
+        -- newPage.Parent = RightPages
+        -- newPage.Active = true
+        -- newPage.BackgroundColor3 = Themes['Default'].ContainerColor
+        -- newPage.BackgroundTransparency = 1.000
+        -- newPage.BorderSizePixel = 0
+        -- newPage.Size = UDim2.new(0, 430, 0, 565)
+        -- newPage.ScrollBarThickness = 8
 
-        -- [Element Listing] New Page
-        elementListing.Name = "elementListing"
-        elementListing.Parent = newPage
-        elementListing.SortOrder = Enum.SortOrder.LayoutOrder
+        -- -- [Element Listing] New Page
+        -- elementListing.Name = "elementListing"
+        -- elementListing.Parent = newPage
+        -- elementListing.SortOrder = Enum.SortOrder.LayoutOrder
 
     end
 
